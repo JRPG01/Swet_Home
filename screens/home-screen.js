feb75b386b27ca9b0cb3ba2e0d40cdcf1f7b5f91
RaphaChi08 <s20120207@alumnos.itsur.edu.mx>
2024-03-27 17:45:21 -0600

    Se avanzó en el Home screen, se esta haciendo el header propío y luego se utilizará flatList para dibujar la lista de casas en forma de tarjetas

--- screens/home-screen.js
+++ screens/home-screen.js
@@ -1,25 +1,42 @@
+import React from "react";
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, StyleSheet, Image, Pressable} from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 const logo = "";
+const fondo = "../resources/fondo_HomeScreen";
 
 export default function HomeScreen () {
+    const [logiado, isLogiado] = useState(false);
+
     const navigation = useNavigation();
+    const [iniciar, setiniciar] = useState(false);
+    const [registrar, setRegistrar] = useState(false);
+
+    const inicioIN = () => setiniciar(true);
+    const inicioOUT = () => setiniciar(false);
+    const registrarIN = () => setRegistrar(true);
+    const registrarOUT = () => setRegistrar(false);
 
     return(
         <View style = {style.screen}>
-            <View style={{width: "100%", height:100, backgroundColor: "#4f6f32"}}>
+            <View style={{width: "100%", height:100, backgroundColor: "#4f6f32", position: "absolute", top:0, left:0, right:0, flexDirection: "row", justifyContent:"flex-end"}}>
+                
                 <Image source={{uri: logo}} />
 
-            </View>
-            
-            <Text>Pantalla Home</Text>
-
-            <View style={style.screen}>
-                <Pressable onPress={()=>navigation.navigate('Login')} style={style.boton}>
-                    <Text>Login</Text>
+                <Pressable onPress={()=>navigation.navigate('Login')} style={style.boton} onPressIn={inicioIN} onPressOut={inicioOUT}>
+                    <Text style={iniciar ? style.textoSeleccionado : style.texto }>Inicia sesión</Text>
+                </Pressable>
+                <Text style={style.texto}>o</Text>
+                <Pressable onPress={()=>navigation.navigate('Register')} style={style.boton} onPressIn={registrarIN} onPressOut={registrarOUT}>
+                    <Text style={registrar ? style.textoSeleccionado : style.texto}>Registrate</Text>
                 </Pressable>
             </View>
+            
+            <FlatList>
+                <Image source={{uri: fondo}} style={[style.screen, StyleSheet.absoluteFillObject]}/>
+            </FlatList>
         </View>
     );
 }
@@ -27,15 +44,29 @@ export default function HomeScreen () {
 const style = StyleSheet.create({
     screen:{
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
     },
 
     boton:{
-        justifyContent: "center",
-        alignItems: "center",
-        margin: 50,
-        width: "100%",
         height: "100%",
-        backgroundColor: "rgba(255, 0, 0, 1)"
+        position: "relative",
+    },
+
+    texto:{
+        height: 100,
+        display: "flex",
+        alignItems:"center",
+        justifyContent: "center",
+        paddingHorizontal: 10,
+        color: "white"
+    },
+
+    textoSeleccionado:{
+        height: 100,
+        display: "flex",
+        alignItems:"center",
+        justifyContent: "center",
+        paddingHorizontal: 10,
+        color: "cyan"
     }
 });
